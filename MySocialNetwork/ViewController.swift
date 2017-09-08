690b54a818b88d9e969498d612203c8c6b16d40b
LNSousa <lucasns241@gmail.com>
2017-09-07 20:05:07 -0400

    Initial Commit

---
+++ MySocialNetwork/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MySocialNetwork
+//
+//  Created by Lucas Sousa on 9/7/17.
+//  Copyright © 2017 Lucas Sousa. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
